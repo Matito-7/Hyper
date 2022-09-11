7826e2f02ac2c7b56f4579fc0947eb72c85ceeb8
Matito-7 <59352894+Matito-7@users.noreply.github.com>
2022-09-11 00:10:06 -0300

    Add files via upload

---
+++ javaScript/PáginaPrincipal/mobile.js
@@ -0,0 +1,18 @@
+let menuMobile = document.querySelector(".mobile-menu")
+let lista = document.querySelector('.ListaNavegacao')
+
+menuMobile.addEventListener('click', openNav)
+
+function openNav(){
+    lista.classList.toggle('dois');
+    console.log(lista)
+}
+
+
+
+
+
+
+
+
+
